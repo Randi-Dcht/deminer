bc89db670a70287dde6a4012204a627ca5f93740
Randi_Tour_Linux <190134@umons.ac.be>
2020-10-18 10:53:33 +0200

    Add the graphics objects

---
+++ src/be/dchrnd/Graphics/CaseG.java
@@ -0,0 +1,53 @@
+package be.dchrnd.Graphics;
+
+import be.dchrnd.Control;
+import be.dchrnd.Objects.Cases;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class CaseG
+{
+    public Cases cases;
+    private Rectangle rectangle;
+    private Group pane;
+
+    public CaseG(Cases cases)
+    {
+        this.cases = cases;
+        pane = new Group();
+    }
+
+    public void click() throws FileNotFoundException
+    {
+        if (cases.isOccupied())
+        {
+            pane.getChildren().remove(rectangle);
+            FileInputStream stream = new FileInputStream("");//TODO
+            Image ig = new Image(stream);
+            ImageView img = new ImageView();
+            img.setImage(ig);
+            img.setFitHeight(Control.HEIGHT);
+            img.setFitWidth(Control.WIDTH);
+            pane.getChildren().add(img);
+        }
+        else
+            rectangle.setFill(Color.GRAY);
+    }
+
+    public Node draw()
+    {
+        pane.prefHeight(Control.HEIGHT);pane.prefWidth(Control.WIDTH);
+        rectangle = new Rectangle();
+        rectangle.setWidth(Control.WIDTH);rectangle.setHeight(Control.HEIGHT);
+        rectangle.setFill(Color.BLACK);
+        pane.getChildren().add(rectangle);
+        return pane;
+    }
+}
