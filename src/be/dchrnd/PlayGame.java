7a53e0f52c8576238afeaea93613840bad940726
Randi_Tour_Linux <190134@umons.ac.be>
2020-10-18 11:36:22 +0200

    add the timer and the score in the play game

--- src/be/dchrnd/PlayGame.java
+++ src/be/dchrnd/PlayGame.java
@@ -3,29 +3,50 @@ package be.dchrnd;
 import be.dchrnd.Graphics.CaseG;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import java.io.FileNotFoundException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class PlayGame extends Application
 {
     private GridPane grid;
     private CaseG[][] board;
+    private Text time;
+    private Text score;
+    private Timer timer;
+
+    private int sec = 0;
+    private int min = 0;
 
     @Override
     public void start(Stage primaryStage)
     {
         grid = new GridPane();
-        Scene scene = new Scene(grid,1200,1200);
+        BorderPane pane = new BorderPane();
+        Scene scene = new Scene(pane,1200,1200);
 
         initBoard();
         drawBoard();
 
+        HBox topBar = new HBox();
+        topBar.setSpacing(50);
+        score = new Text();score.setText("0");
+        time  = new Text();time.setText(String.format("%d:%d",min,sec));
+        topBar.getChildren().addAll(score,time);
+
+        pane.setCenter(grid);
+        pane.setTop(topBar);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Play to deminer");
         primaryStage.show();
 
         grid.setOnMouseClicked(e->calculusClick(e.getX(),e.getY()));
+        timer = new Timer();timer.schedule(task,1000,1000);
     }
 
     private void calculusClick(double x, double y)
@@ -69,4 +90,19 @@ public class PlayGame extends Application
                 grid.add(board[j][i].draw(),j,i);
         }
     }
+
+    TimerTask task = new TimerTask()
+    {
+        @Override
+        public void run()
+        {
+            sec++;
+            if (sec == 60)
+            {
+                sec =0;
+                min++;
+            }
+            time.setText(String.format("%d:%d",min,sec));
+        }
+    };
 }
