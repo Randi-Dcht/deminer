751322eff3a63c2aa78386dbff7bfdce800153e3
Randi_Tour_Linux <190134@umons.ac.be>
2020-10-18 10:55:00 +0200

    Add the controller of the game (Supervisor)

---
+++ src/be/dchrnd/Control.java
@@ -0,0 +1,67 @@
+package be.dchrnd;
+
+import be.dchrnd.Objects.Board;
+import be.dchrnd.Objects.Cases;
+
+import java.util.Random;
+
+public class Control
+{
+    /**
+     * The all variables of the program
+     * Size of the node
+     * Size between two nodes
+     * Size of the board
+     */
+    public static int WIDTH  = 50;//node
+    public static int HEIGHT = 50;//node
+    public static int SPACEX = 3;//space
+    public static int SPACEY = 3;//space
+    public static int size;//board
+    /**
+     * The board of the game (Logic)
+     */
+    private static Board board;
+    /**
+     * The random
+     */
+    private static Random rd = new Random();
+
+    public static void initLogicBoard(int size_,int sizeCase)
+    {
+        board = new Board(size_);
+        size = size_;
+        WIDTH = HEIGHT = sizeCase;
+    }
+
+    public static void placeBomb(int number)
+    {
+        int cnt = 0;
+        while (cnt < number)
+        {
+            int randX = random(0,size);
+            int randY = random(0,size);
+
+            if (! board.getCase(randX,randY).isOccupied())
+            {
+                board.getCase(randX,randY).putObject();
+                cnt++;
+            }
+        }
+        board.print();//TODO debug
+    }
+
+    public static int random(int min, int max)
+    {
+        int n = rd.nextInt(max);
+        if (n >= min && n <= max)
+            return n;
+        else
+            return random(min,max);
+    }
+
+    public static Cases getCase(int x, int y)
+    {
+        return board.getCase(x,y);
+    }
+}
